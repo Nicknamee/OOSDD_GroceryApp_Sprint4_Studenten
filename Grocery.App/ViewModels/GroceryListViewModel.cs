7e93feae9252d332b070fc820396b444320e4480
Nick <42804013+Nicknamee@users.noreply.github.com>
2025-10-03 15:43:13 +0200

    Add admin navigation command to grocery list view model

--- Grocery.App/ViewModels/GroceryListViewModel.cs
+++ Grocery.App/ViewModels/GroceryListViewModel.cs
@@ -1,7 +1,9 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Grocery.Core.Enums;
 using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace Grocery.App.ViewModels
@@ -10,11 +12,16 @@ namespace Grocery.App.ViewModels
     {
         public ObservableCollection<GroceryList> GroceryLists { get; set; }
         private readonly IGroceryListService _groceryListService;
+        private readonly GlobalViewModel _global;
 
-        public GroceryListViewModel(IGroceryListService groceryListService) 
+        [ObservableProperty]
+        private Client? client;
+
+        public GroceryListViewModel(IGroceryListService groceryListService, GlobalViewModel globalViewModel)
         {
             Title = "Boodschappenlijst";
             _groceryListService = groceryListService;
+            _global = globalViewModel;
             GroceryLists = new(_groceryListService.GetAll());
         }
 
@@ -24,9 +31,11 @@ namespace Grocery.App.ViewModels
             Dictionary<string, object> paramater = new() { { nameof(GroceryList), groceryList } };
             await Shell.Current.GoToAsync($"{nameof(Views.GroceryListItemsView)}?Titel={groceryList.Name}", true, paramater);
         }
+
         public override void OnAppearing()
         {
             base.OnAppearing();
+            Client = _global.Client;
             GroceryLists = new(_groceryListService.GetAll());
         }
 
@@ -34,6 +43,17 @@ namespace Grocery.App.ViewModels
         {
             base.OnDisappearing();
             GroceryLists.Clear();
+        }
+
+        [RelayCommand]
+        private async Task ShowBoughtProducts()
+        {
+            if (Client?.Role != Role.Admin)
+            {
+                return;
+            }
+
+            await Shell.Current.GoToAsync(nameof(Views.BoughtProductsView));
         }
     }
 }
